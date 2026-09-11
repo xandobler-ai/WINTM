68497b9f4dc52dcc0d87ef5f9ff14ea9b7c47013
xandobler-ai <xandobler@gmail.com>
2026-09-11 23:04:11 +1000

    Update Memory.h

--- src/core/Memory.h
+++ src/core/Memory.h
@@ -10,6 +10,9 @@ public:
 
     bool allocate(std::size_t megabytes);
 
+    bool write(std::size_t address, unsigned char value);
+    unsigned char read(std::size_t address) const;
+
     void clear();
 
     std::size_t getSize() const;
