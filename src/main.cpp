3d54032a61b96a6e10b7d6854cf040b8324b862d
xandobler-ai <xandobler@gmail.com>
2026-09-11 23:02:33 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
+
 #include "core/VM.h"
 
 int main()
 {
-    std::cout << "WinVM starting...\n";
+    std::cout << "====================\n";
+    std::cout << "       WinVM\n";
+    std::cout << "====================\n\n";
 
     VM vm;
 
-    if (!vm.create(512))
+    if (!vm.create(16))
     {
         std::cerr << "Failed to create VM.\n";
         return 1;
     }
 
-    std::cout << "VM created successfully.\n";
-    std::cout << "RAM: " << vm.getMemoryMB() << " MB\n";
+    std::cout << "VM created.\n";
+    std::cout << "RAM: "
+              << vm.getMemoryMB()
+              << " MB\n\n";
+
+    /*
+        Our tiny CPU instruction set:
+
+        0x00 = NOP
+        0x01 = HALT
+        0x02 = INC R0
+        0x03 = DEC R0
+
+        Program:
+
+        INC R0
+        INC R0
+        INC R0
+        HALT
+    */
+
+    // We need access to RAM for our test program.
+    // For now, we'll add a temporary way through Memory.
+    // This will be improved when we implement the memory bus.
 
     vm.start();
 
-    std::cout << "VM is running: "
-              << (vm.isRunning() ? "yes" : "no")
+    std::cout << "VM started.\n";
+
+    std::cout << "CPU R0: "
+              << vm.getCPU().getRegister(0)
               << "\n";
 
     vm.stop();
